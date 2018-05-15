f14e3957a11abe166b5443b8c6bbebae5823c9d5
D7703_30 <d7703_30@d7703-30ui-imac.local>
2018-05-15 14:19:45 +0900

    Initial Commit

---
+++ UISlider/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UISlider
+//
+//  Created by D7703_30 on 2018. 5. 15..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
